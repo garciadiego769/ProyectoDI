80fff8bd97233cec0b7839550c75d8fbea7a4b30
Iván Mejía <ivanmejiapulido@gmail.com>
2018-11-27 21:47:42 +0100

    Creado proyecto AccesoCSV
    Añadida región en program.cs para AccesoCSV.

--- Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
+++ Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
@@ -12,12 +12,16 @@ namespace ProyectoComunDI
         {
             Console.WriteLine("Hola Mundo");
 
-            #region Acceso MySQL
+			#region Acceso MySQL
 
-            // cread una region cada uno con un ejemplo de como se accedea vuestra libreria
+			// cread una region cada uno con un ejemplo de como se accedea vuestra libreria
 
-            #endregion
+			#endregion
 
-        }
-    }
+			#region AccesoCSV
+
+			#endregion
+
+		}
+	}
 }
