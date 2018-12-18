95c4977091bbad4c2c05f3189a2c0ae0e6a1df9d
ARMASJOSE <armasjose1980@gmail.com>
2018-12-18 23:21:18 +0100

    Creada la clase ConexionBd

---
+++ Desarrollos/ProyectoComunDI/AccesoSqlServer/ConexionBd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoSqlServer
+{
+    class ConexionBd
+    {
+
+        //Atributo de tipo SqlConecction para conectar al sevidor y a la BD
+        private SqlConnection con = new SqlConnection("server=JOSE\\SERVIDOR" +
+                " ; database=Prueba ; integrated security = true");
+
+        //Metodo para abrir la conexion
+        public void abrirConexion()
+        {
+
+            try
+            {
+                con.Open();
+                Console.WriteLine("Conexión realizada con éxito");
+            }
+            catch (SqlException e)
+            {
+                //Lanzo mensaje de excepcion
+                throw e;
+            }
+
+        }
+
+        //Metodo para cerrar la conexion
+        public void cerrarConexion()
+        {
+            try
+            {
+                con.Close();
+                Console.WriteLine("Conexión cerrada con éxito");
+            }
+            catch (SqlException e)
+            {
+                //Lanzo mensaje de excepcion
+                throw e;
+            }
+
+        }
+    }
+}
--- Desarrollos/ProyectoComunDI/AccesoSqlServer/Program.cs
+++ Desarrollos/ProyectoComunDI/AccesoSqlServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,30 @@ namespace AccesoSqlServer
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+
+            //Creo objeto tipo ConexionBd
+            ConexionBd conexionBd = new ConexionBd();
+
+            try
+            {
+                //Abro conexion
+                conexionBd.abrirConexion();
+               
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            
+
+        
+
+
+
         }
     }
 }
