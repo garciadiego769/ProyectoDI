7585f896b11634856a80d3417295bcbc594c9b57
Unknown <ivanmejiapulido@gmail.com>
2019-01-15 21:31:21 +0100

    Agregado proyecto Utilidades
    Agregada clase IRIS

--- Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
+++ Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AccesoCSV;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace ProyectoComunDI
     {
         static void Main(string[] args)
         {
+			Acceso test = new Acceso();
+			test.downloadFile();
             Console.WriteLine("Hola Mundo");
 
 			#region Acceso MySQL
