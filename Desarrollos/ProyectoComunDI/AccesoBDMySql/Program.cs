0cb069d42723243f6c7605d97ad70146fcdc7fab
davidmr <david.montero.rajado@gmail.com>
2019-01-15 21:54:24 +0100

    Primer boceto del proyecto

--- Desarrollos/ProyectoComunDI/AccesoBDMySql/Program.cs
+++ Desarrollos/ProyectoComunDI/AccesoBDMySql/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccesoBDMySql;
 
 namespace AccesoBDMySql
 {
@@ -10,6 +11,17 @@ namespace AccesoBDMySql
     {
         static void Main(string[] args)
         {
+
+            List<string[]> dataseti = new List<string[]>();
+            dataseti = dataset.cargarDatos("127.0.0.1", "3306", "root","", "dataset");
+
+            foreach(string[] row in dataseti)
+            {
+                Console.WriteLine(row[0] + " " + row[1] + " " + row[2] + " " + row[3] + " " + row[4]);
+            }
+            Console.ReadKey();
+
+
         }
     }
 }
