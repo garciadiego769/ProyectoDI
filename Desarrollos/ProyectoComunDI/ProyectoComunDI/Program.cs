17e4786f01206c65000e93bf1007669e245fdd97
Iker Arana <iarana@egibide.org>
2019-01-29 19:45:56 +0100

    Cambio Proyecto
    Añadida la parte de Eric y Jose

--- Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
+++ Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
@@ -16,15 +16,9 @@ namespace ProyectoComunDI
 
             #region Acceso MySQL
 
+            // cread una region cada uno con un ejemplo de como se accedea vuestra libreria
+
             #endregion
-            #region AccesoCSV
-
-			#endregion
-
-			#region Configurador
-
-			// aquí se instancian las clases necesarias para conectarse a cada base de datos.
-			#endregion
 
             #region Aceso SQLServer
             //Lista de evidencias            
@@ -44,5 +38,4 @@ namespace ProyectoComunDI
 
         }
     }
-
 }
