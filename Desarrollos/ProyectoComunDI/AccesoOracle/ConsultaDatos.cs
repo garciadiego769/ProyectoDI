60ebb69646031d96bfc7f6e8468f62dfd5ac175a
Diego <garciadiego769@gmail.com>
2019-02-05 19:11:41 +0100

    Conexion hecha y devuelve datos tipo Evidencia

---
+++ Desarrollos/ProyectoComunDI/AccesoOracle/ConsultaDatos.cs
@@ -0,0 +1,66 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Collections.Generic;
+using System.Data.Common;
+namespace AccesoOracle
+
+{
+
+    public static class ConsultaDatos
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static List<Evidencia> AccesoBDOracle()
+        {
+            List<Evidencia> evidencias = new List<Evidencia>();
+            const string V = "User Id=SYSTEM;Password=12345Abcde;";
+            string oradb = "Data Source=(DESCRIPTION="
+             + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=PC-DIEGO)(PORT=1521)))"
+             + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));"
+             + V;
+            OracleConnection conexion = new OracleConnection(oradb);
+
+            conexion.Open();
+            //Console.WriteLine("Connected to Oracle" + conexion.ServerVersion);
+
+            //SELECT
+            string sql = "select * from DANIDIEGO.ATRIBUTOSFLORES";
+            OracleCommand cmd = new OracleCommand();
+            cmd.CommandText = sql;
+            cmd.Connection = conexion;
+
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+
+                while (reader.Read())
+                {
+                    double longitudPetalo = reader.GetDouble(2);
+                    // Console.WriteLine("Longitud Pétalo: " + longitudPetalo.ToString("0.00"));
+
+                    double longitudSepalo = reader.GetDouble(3);
+                    // Console.WriteLine("Longitud Sépalo: " + longitudSepalo.ToString("0.00"));
+
+                    double anchoPetalo = reader.GetDouble(4);
+                    // Console.WriteLine("Ancho Pétalo: " + anchoPetalo.ToString("0.00"));
+
+                    double anchoSepalo = reader.GetDouble(5);
+                    //Console.WriteLine("Ancho Sépalo: " + anchoSepalo.ToString("0.00"));
+
+                    string tipoFlor = reader.GetString(6);
+                    // Console.WriteLine("Tipo Flor: " + tipoFlor);
+
+                    //Creamos los datos como objeto del tipo Evidencia
+                    Evidencia evidencia = new Evidencia(longitudPetalo, longitudSepalo, anchoPetalo, anchoSepalo, tipoFlor);
+
+                    evidencias.Add(evidencia);
+
+                }
+                return evidencias;
+            }
+
+
+        }
+    }
+}
--- Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
+++ Desarrollos/ProyectoComunDI/ProyectoComunDI/Program.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Oracle.ManagedDataAccess.Client;
-
-
-namespace ProyectoComunDI
+﻿namespace ProyectoComunDI
 {
-  
+
     class Program
     {
-        private const string V = "User Id=SYSTEM;Password=12345Abcde;";
+
         static void Main(string[] args)
         {
             //Console.WriteLine("Hola Mundo");
@@ -24,53 +15,10 @@ namespace ProyectoComunDI
             #endregion
 
             #region Acceso Oracle
-
-            //connection.ConnectionString = "User Id=<SYSTEM>;Password=<12345Abcde>;Data Source=<DANIDIEGO>"; //Data Source Format -> //IP_HOST:PORT/SERVICE_NAME e.g. //127.0.0.1:1521/Service_Name
-
-            string oradb = "Data Source=(DESCRIPTION="
-             + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=PC-DIEGO)(PORT=1521)))"
-             + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));"
-             + V;
-            OracleConnection conexion = new OracleConnection(oradb);
-
-            conexion.Open();
-            Console.WriteLine("Connected to Oracle" + conexion.ServerVersion);
-
-            //SELECT
-            string sql = "select * from DANIDIEGO.ATRIBUTOSFLORES";
-            OracleCommand cmd = new OracleCommand();
-            cmd.CommandText = sql;
-            cmd.Connection = conexion;
-            
-            /*SqlDataReader dr1 = cmd.ExecuteReader();
-            if (dr1.Read())
-            {
-                Label8.Text = dr1["quest"].ToString();
-                Label9.Text = dr1["ans1"].ToString();
-            }
-            cn.Close();*/
-
-
-            /////
-            using (DbDataReader reader = cmd.ExecuteReader())
-            {
-                // OracleCommand sqlCmd = new OracleCommand(sql, conexion);
-                string tipoFlor = reader.GetString(2);
-
-                Console.WriteLine(tipoFlor);
-
-            }
-
-            
-
-            
-           // OracleDataReader dr = sqlCmd.ExecuteReader();
-            //dr.Read();
-            Console.Read();
+            /*
+             * AccesoOracle.ConsultaDatos.AccesoBDOracle();
+             */
             #endregion
-
-           
-
 
         }
     }
